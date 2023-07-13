c8212f134ed35206387ff9fab3e103fe52830c29
ElPlaso <plahsoekusay@gmail.com>
2023-07-13 14:45:02 +1200

    add slider.

--- src/components/SongPlayer.tsx
+++ src/components/SongPlayer.tsx
@@ -1,11 +1,13 @@
-import { useState, useRef, ChangeEvent } from "react";
+import { useState, useRef, ChangeEvent, useEffect } from "react";
 
 /**
  * @returns A component that allows the user to select a file and play it.
  */
 export default function SongPlayer() {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [currentTime, setCurrentTime] = useState(0);
   const audioRef = useRef<HTMLAudioElement | null>(null);
+  const isLoaded = useRef(false); // Track whether the audio is loaded
 
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files && event.target.files[0];
@@ -20,15 +22,66 @@ export default function SongPlayer() {
     audioRef.current?.pause();
   };
 
+  const handleSliderChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const time = parseFloat(event.target.value);
+    setCurrentTime(time);
+    if (audioRef.current) audioRef.current.currentTime = time;
+  };
+
+  useEffect(() => {
+    const handleTimeUpdate = () => {
+      if (audioRef.current) setCurrentTime(audioRef.current.currentTime);
+    };
+
+    audioRef.current?.addEventListener("timeupdate", handleTimeUpdate);
+
+    return () => {
+      audioRef.current?.removeEventListener("timeupdate", handleTimeUpdate);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!audioRef.current || !selectedFile) return;
+
+    audioRef.current.src = URL.createObjectURL(selectedFile);
+
+    // Reset the current time when the audio is loaded
+    const handleLoadedMetadata = () => {
+      setCurrentTime(0);
+      isLoaded.current = true;
+    };
+
+    audioRef.current.addEventListener("loadedmetadata", handleLoadedMetadata);
+
+    return () => {
+      audioRef.current?.removeEventListener(
+        "loadedmetadata",
+        handleLoadedMetadata
+      );
+    };
+  }, [selectedFile]);
+
+  useEffect(() => {
+    // Play the audio after it's loaded if it was already playing
+    if (isLoaded.current) {
+      handlePlay();
+    }
+  }, [isLoaded]);
+
   return (
     <div>
       <input type="file" onChange={handleFileChange} />
-      <audio
-        ref={audioRef}
-        src={selectedFile ? URL.createObjectURL(selectedFile) : undefined}
-      />
+      <audio ref={audioRef} />
       <button onClick={handlePlay}>Play</button>
       <button onClick={handlePause}>Pause</button>
+      <input
+        type="range"
+        min={0}
+        max={audioRef.current?.duration}
+        value={currentTime}
+        onChange={handleSliderChange}
+      />
+      <span>{currentTime.toFixed(2)}</span>
     </div>
   );
 }
